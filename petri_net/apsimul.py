072a5d775d683f35bd5b428955239eab1400cc13
Aytar Akdemir <aytarakdemir99@gmail.com>
2022-06-11 22:32:08 +0300

    implemented an simpler state input method

--- petri_net/apsimul.py
+++ petri_net/apsimul.py
@@ -1,37 +1,37 @@
 from petri import PetriNet
 
 # View photo 5 times and no more
-petri = PetriNet('net_config/photo_view_limit.txt', 0.5)
+petri = PetriNet('net_config/photo_view_limit.txt')
 petri.printPlaceTokens()
-petri.fire(0)
-petri.fire(0)
-petri.fire(0)
-petri.fire(0)
-petri.fire(0)
-petri.fire(0)
-petri.fire(0)
+petri.fire("access")
+petri.fire("access")
+petri.fire("access")
+petri.fire("access")
+petri.fire("access")
+petri.fire("access")
+petri.fire("access")
 petri.printPlaceTokens()
 
 # Infinite token
 petri2 = PetriNet('net_config/infinite_token_loop.txt')
 petri2.printPlaceTokens()
-petri2.fire(0)
-petri2.fire(0)
-petri2.fire(0)
+petri2.fire("access")
+petri2.fire("access")
+petri2.fire("access")
 petri2.printPlaceTokens()
 
 # Give permission to read for 2 times only
 petri3 = PetriNet('net_config/give_permission.txt')
 petri3.printPlaceTokens()
-petri3.fire(1) # Cannot fire
+petri3.fire("access") # Cannot fire
 petri3.printPlaceTokens()
-petri3.fire(0) # Give permission for twice
+petri3.fire("givePermission") # Give permission for twice
 petri3.printPlaceTokens()
-petri3.fire(1) # Can read
+petri3.fire("access") # Can read
 petri3.printPlaceTokens()
-petri3.fire(1) # Can read again
+petri3.fire("access") # Can read again
 petri3.printPlaceTokens()
-petri3.fire(1) # Cannot read anymore
+petri3.fire("access") # Cannot read anymore
 
 
 """ Pull request review process: at least 3 people need to approve in order for PR request to be accepted. If the previous PR is not accepted
@@ -39,27 +39,27 @@ then the a new PR cannot be created.
 """
 petri4 = PetriNet('net_config/pull_request_review.txt')
 petri4.printPlaceTokens()
-petri4.fire(0)
+petri4.fire("createPR")
 petri4.printPlaceTokens()
-petri4.fire(2)
-petri4.fire(2)
-petri4.fire(2)
+petri4.fire("approvePR")
+petri4.fire("approvePR")
+petri4.fire("approvePR")
 petri4.printPlaceTokens()
-petri4.fire(1)
+petri4.fire("push")
 petri4.printPlaceTokens()
 
 
 # Give permission to write and take away permission (can also grant permission to give others permission)
 petri5 = PetriNet('net_config/grant_and_revoke_permission.txt')
 petri5.printPlaceTokens()
-petri5.fire(1)
-petri5.fire(1) # Cannot write
-petri5.fire(2) # Cannot revoke since there is no permission anyway
-petri5.fire(0) # Give permission to write
-petri5.fire(1) # Write
-petri5.fire(1) # Write as much as you like :)
-petri5.fire(2) # Permission revoked
-petri5.fire(1) # Cannot write :(
+petri5.fire("access")
+petri5.fire("access") # Cannot write
+petri5.fire("revokePermission") # Cannot revoke since there is no permission anyway
+petri5.fire("grantPermission") # Give permission to write
+petri5.fire("access") # Write
+petri5.fire("access") # Write as much as you like :)
+petri5.fire("revokePermission") # Permission revoked
+petri5.fire("access") # Cannot write :(
 
 
 
--- petri_net/petri.py
+++ petri_net/petri.py
@@ -5,13 +5,14 @@ Examine https://en.wikipedia.org/wiki/Petri_net to undestand what classes do.
 import random
 
 class Place:
-    def __init__(self, index, token):
-        self.index = index
+    def __init__(self, name, token):
+        self.name = name
         self.token = token
 
 
 class Transition:
-    def __init__(self, input_places, output_places):
+    def __init__(self, name, input_places, output_places):
+        self.name = name
         self.input_places = input_places
         self.output_places = output_places
 
@@ -32,7 +33,8 @@ class Transition:
 
 # Arc that goes  place --> transition
 class InputPlace:
-    def __init__(self, place, tokens_to_be_inputted = 1):
+    def __init__(self, id, place, tokens_to_be_inputted = 1):
+        self.id = id
         self.place = place
         self.tokens_to_be_inputted = tokens_to_be_inputted 
 
@@ -45,7 +47,8 @@ class InputPlace:
 
 # Arc that goes  transition --> place
 class OutputPlace:
-    def __init__(self, place, tokens_to_be_outputted = 1):
+    def __init__(self, id, place, tokens_to_be_outputted = 1):
+        self.id = id
         self.place = place
         self.tokens_to_be_outputted = tokens_to_be_outputted
 
@@ -56,122 +59,129 @@ class OutputPlace:
 class PetriNet:
     def __init__(self, filename, random_privilege_escalation_probability = 0.0):
         self.random_privilege_escalation_probability = random_privilege_escalation_probability
+
+        places_from_txt = []
+        inputs_from_txt = []
+        outputs_from_txt = []
+        transitions_from_txt = []
+
         with open(filename) as f:    
             lines = f.readlines()
-            a = lines[0][:-1].split(' ')
-            a = list(map(int, a))
+            id = 0
+            for cur_line in lines:
+                cur_line.replace(" ", "")
+                if (cur_line[-1] != '\n'):
+                    cur_line += '\n' 
+                if (cur_line[0] == '/'):
+                    if(cur_line[1:6] == "place"):
+                        inside = cur_line[7:-2].split(",")
+                        place_info = {
+                            "name": inside[0],
+                            "token": int(inside[1])
+                        }
+                        places_from_txt.append(place_info)
+                    elif(cur_line[1:11] == "transition"):
+                        inside = cur_line[12:-2].split(",")
+                        transition_info = {
+                            "name": inside[0],
+                        }
+                        transitions_from_txt.append(transition_info)
+                    elif(cur_line[1:5] == "PtoT"):
+                        inside = cur_line[6:-2].split(",")
+                        input_info = {
+                            "id": id,
+                            "P": inside[0],
+                            "T": inside[1],
+                            "token_minus": int(inside[2])
+                        }
+                        id += 1
+                        inputs_from_txt.append(input_info)
+                    elif(cur_line[1:5] == "TtoP"): 
+                        inside = cur_line[6:-2].split(",")
+                        output_info = {
+                            "id": id,
+                            "T": inside[0],
+                            "P": inside[1],
+                            "token_plus": int(inside[2])
+                        }
+                        id += 1
+                        outputs_from_txt.append(output_info)
+                    else:
+                        print("Error: Undefined command")
 
-
-            #inputs
-            if(lines[1] != '\n'):
-                bc = lines[1].split('-')
-                b = bc[0].split(' ')
-                if (b[-1] == ''):
-                    b = b[:-1]
-                c = bc[1][:-1].split(' ')
-                if (c[0] == ''):
-                    c = c[1:]
-                b = list(map(int, b))
-                c = list(map(int, c))
-            else:
-                b = []
-                c = []
-
-            # outputs
-            if(lines[2] != '\n'):
-                de = lines[2].split('-')
-                d = de[0].split(' ')
-                if (d[-1] == ''):
-                    d = d[:-1]
-                e = de[1][:-1].split(' ')
-                if (e[0] == ''):
-                    e = e[1:]
-                d = list(map(int, d))
-                e = list(map(int, e))
-            else:
-                d = []
-                e = []
-
-            f = lines[3][:-1].split(' ')
-            f = list(map(int, f))
-
-            if(lines[4][-1] == '\n'):
-                g = lines[4][:-1].split(' ')
-            else:
-                g = lines[4].split(' ')
-            g = list(map(int, g))
-
-            place_tokens = a
-
-            input_req_token = b
-            input_place_list = c
-
-            output_out_token = d
-            output_place_list = e
-
-            transition_input_count = f
-            transition_output_count = g
+            
 
             # Set token
             self.places = []
-            for i in range(len(place_tokens)):
-                place_instance = Place(i, place_tokens[i])
+            for i in range(len(places_from_txt)):
+                place_instance = Place(places_from_txt[i]["name"], places_from_txt[i]["token"])
                 self.places.append(place_instance)
 
 
             self.input = []
-            for i in range(len(input_req_token)):
-                input_instance = InputPlace(self.places[input_place_list[i]], input_req_token[i])
-                self.input.append(input_instance)
+            for i in range(len(inputs_from_txt)):
+                for a in range(len(self.places)):
+                    if (self.places[a].name == inputs_from_txt[i]['P']):
+                        input_instance = InputPlace(inputs_from_txt[i]['id'], self.places[a], inputs_from_txt[i]['token_minus'])
+                        self.input.append(input_instance)
 
 
             self.output = []
-            for i in range(len(output_out_token)):
-                output_instance = OutputPlace(self.places[output_place_list[i]], output_out_token[i])
-                self.output.append(output_instance)
+            for i in range(len(outputs_from_txt)):
+                for a in range(len(self.places)):
+                    if (self.places[a].name == outputs_from_txt[i]['P']):
+                        output_instance = OutputPlace(outputs_from_txt[i]['id'], self.places[a], outputs_from_txt[i]['token_plus'])
+                        self.output.append(output_instance)
 
 
             self.transitions = []
-            temp_input = []
-            i = 0
-            a = 0 
-            while i < len(transition_input_count):
-                temp_input.append(self.input[a:a+transition_input_count[i]])
-                a = a + transition_input_count[i]
-                i += 1
 
-
-            temp_output = []
-            j = 0
-            b = 0
-            while j < len(transition_output_count):
-                temp_output.append(self.output[b: b+transition_output_count[j]])
-                b = b + transition_output_count[j]
-                j += 1
-
-
-            for i in range(len(temp_input)):
-                transition_instance = Transition(temp_input[i], temp_output[i])
+            for i in range(len(transitions_from_txt)):
+                temp_input = []
+                temp_output = []
+                
+                for z in inputs_from_txt:
+                    if (z['T'] == transitions_from_txt[i]['name']):
+                        for j in self.input:
+                            if (j.id == z['id']):
+                                temp_input.append(j)
+                for z in outputs_from_txt:        
+                    if (z['T']  == transitions_from_txt[i]['name']):
+                        for j in self.output:
+                            if (j.id == z['id']):
+                                temp_output.append(j)
+                transition_instance = Transition(transitions_from_txt[i]['name'], temp_input, temp_output)
                 self.transitions.append(transition_instance)
 
 
-    def fire(self, transition_id, fire_unconditionally = False):
+
+
+            
+
+
+    def fire(self, transition_name, fire_unconditionally = False):
         
         if (random.random() < self.random_privilege_escalation_probability):
             fire_unconditionally = True
             
-        if (self.transitions[transition_id].fire(fire_unconditionally) == 0):
-            print("Fired transition")
-        elif (self.transitions[transition_id].fire(fire_unconditionally) == 1):
-            print("Fired transition with unauthorized privilege escalation")
-        else:
-            print("Transition could not be fired.")
+        for i in self.transitions:
+            if (i.name == transition_name):
+                if (i.fire(fire_unconditionally) == 0):
+                    print("'" + transition_name + "': Fired transition")
+                elif (i.fire(fire_unconditionally) == 1):
+                    print("'" + transition_name + "': Fired transition with unauthorized privilege escalation")
+                else:
+                    print("'" + transition_name + "': Transition could not be fired.")
 
 
     def printPlaceTokens(self):
         print("---------")
         for i in self.places:
-            print("Place " + str(i.index) + " -> " + str(i.token) + " tokens")
+            print("Place: " + str(i.name) + " -> " + str(i.token) + " tokens")
+        # for i in self.transitions:
+        #     print("Transition: " + i.name)
+        #     for a in i.input_places:
+        #         print(a.place.name)
         print("---------")
         
 
