f02369f7ae47ae8d1680d4c130b23507e7540b53
aytarakdemir <aytarakdemir99@gmail.com>
2023-01-30 19:23:26 +0300

    flat tree created

--- petri_net/analysis.py
+++ petri_net/analysis.py
@@ -1,5 +1,6 @@
 from petri import PetriNet
 import random
+from anytree import Node, RenderTree
 
 petri = PetriNet('net_config/pull_request_review.txt')
 
@@ -30,44 +31,68 @@ def fireAllBFS(depth):
 
     queue.append(petri.saveState())
 
+
+
     for level in range(depth):
         print("LEVEL: " + str(level))
         petri.setState(queue[0])
+        print("QQQQQQQQQQQ:" + str(queue[0]))
+        transitions_this_depth = []
+        #number_of_branches = 0
         for transition_name in transition_names_list:
             print(transition_name)
             if (petri.transitionFirable(transition_name)):
+                #number_of_branches += 1
                 petri.fire(transition_name)
                 trigger_history.append(transition_name)
                 queue.append(petri.saveState())
                 petri.setState(queue[-1])
+        trigger_history.append("*")
         queue.pop(0)
 
     print(trigger_history)
 
 
-    # petri.fire("createPR")
-    # petri.fire("approvePR")
-    # queue.append(petri.saveState())
-    # petri.fire("approvePR")
-    # queue.append(petri.saveState())
-    # petri.fire("approvePR")
-    # petri.fire("push")
-    # petri.printPlaceTokens()
-    # petri.setState(queue.pop())
-    # petri.printPlaceTokens()
+
+    triggers_flat = []
+    temp = []
+    id = 0
+    for item in trigger_history:
+        if (item != '*'):
+            temp.append(item)
+        else:
+            triggers_flat.append(temp)
+            temp = []
+        id += 1
+
+    print(triggers_flat)
+    return triggers_flat
 
 
-    # for i in range(steps):
-    #     print("Iteration: " + str(i))
-    #     firablesArray = []
-    #     for transition_name in transition_names_list:
-    #         firablesArray.append(petri.transitionFirable(transition_name))
-    #     print(firablesArray)
-    #     transitionFirableStatus = {transition_names_list[i]: firablesArray[i] for i in range(len(transition_names_list))}
-    #     print(transitionFirableStatus)
+
+
+def createTree(triggers_flat):
+    root = Node("Root")
+    temp_fork_length = 0
+    for i in range(len(triggers_flat)):
+        if (i == 0):
+            for item in triggers_flat[i]:
+                a = Node(item, root)
+        else:
+            for item in triggers_flat[i]:
+                b = Node(item, root)
+
+
+    for pre, fill, node in RenderTree(root):
+        print("%s%s" % (pre, node.name))
+
+
+
+
+
 
 
 depth = 9
 # print("Trigger History: \n" + str(fireAllRandom(steps)))
 
-fireAllBFS(depth)
+createTree(fireAllBFS(depth))
